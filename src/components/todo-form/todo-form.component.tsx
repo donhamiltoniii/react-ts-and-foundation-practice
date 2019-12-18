0b278d53d29e2921f44d01253401775cfd279fa5
Don Hamilton III <donhamiltoniii@gmail.com>
2019-12-17 21:40:03 -0500

    Add dynamic styling on CustomButton component

---
+++ src/components/todo-form/todo-form.component.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import CustomButton, {
+  CustomButtonColors
+} from "../custom-button/custom-button.component";
+
+interface TodoFormProps {
+  onAddTodo: (text: string) => void;
+}
+
+const TodoForm: React.FC<TodoFormProps> = ({ onAddTodo }) => {
+  const todoInputRef = React.useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const newTodoText = todoInputRef.current!.value;
+    onAddTodo(newTodoText);
+    todoInputRef.current!.value = "";
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        New Todo: <input type="text" ref={todoInputRef} />
+      </label>
+      <CustomButton color={CustomButtonColors.PRIMARY}>ADD TODO</CustomButton>
+    </form>
+  );
+};
+
+export default TodoForm;
