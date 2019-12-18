d5f05b0374be2fc309eb2807a37bd56444317652
Don Hamilton III <donhamiltoniii@gmail.com>
2019-12-17 20:37:38 -0500

    Add color as a prop to custom button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
+import { Colors } from "react-foundation";
+
 import CustomButton from "./components/button/custom-button.component";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <CustomButton>EMPUJAME</CustomButton>
+      <CustomButton color={Colors.ALERT}>EMPUJAME</CustomButton>
+      <CustomButton color={Colors.PRIMARY}>EMPUJAME</CustomButton>
+      <CustomButton color={Colors.SECONDARY}>EMPUJAME</CustomButton>
+      <CustomButton color={Colors.SUCCESS}>EMPUJAME</CustomButton>
+      <CustomButton color={Colors.WARNING}>EMPUJAME</CustomButton>
     </div>
   );
 };
