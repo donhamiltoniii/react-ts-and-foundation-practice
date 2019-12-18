6818613b5ddcb48b6148e96badd1a278551120a1
Don Hamilton III <donhamiltoniii@gmail.com>
2019-12-17 21:03:13 -0500

    Add TodoList components and interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 
-import { Colors } from "react-foundation";
-
-import CustomButton from "./components/button/custom-button.component";
+import Todo from "./interfaces/todo";
+import TodoForm from "./components/todo-form/todo-form.component";
+import TodoList from "./components/todo-list/todolist.component";
 
 const App: React.FC = () => {
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+
+  const onAddTodo = (text: string) => {
+    setTodos(previousTodos => {
+      return [...previousTodos, { id: Math.random().toString(), text }];
+    });
+  };
+
   return (
     <div className="App">
-      <CustomButton color={Colors.ALERT}>EMPUJAME</CustomButton>
-      <CustomButton color={Colors.PRIMARY}>EMPUJAME</CustomButton>
-      <CustomButton color={Colors.SECONDARY}>EMPUJAME</CustomButton>
-      <CustomButton color={Colors.SUCCESS}>EMPUJAME</CustomButton>
-      <CustomButton color={Colors.WARNING}>EMPUJAME</CustomButton>
+      <TodoForm onAddTodo={onAddTodo} />
+      <TodoList todos={todos} />
     </div>
   );
 };
